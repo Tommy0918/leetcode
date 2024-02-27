176e76f48d8ae3817af7bfe82f8bda5598f6c9d0
rookieman <73873398+Tommy0918@users.noreply.github.com>
2024-02-27 23:33:42 +0800

    Time: 7 ms (99.33%), Space: 33.1 MB (22.56%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.cpp
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cpp
@@ -4,7 +4,7 @@ public:
         int n = words.size();
         vector<int> ans;
         for(auto i = 0;i < n;i++){
-            if(words[i].find(x) != string::npos){
+            if(words[i].find(x) != string::npos){ // string::npos means if it cant find the char it will return npos
                 ans.push_back(i);
             }
         }
