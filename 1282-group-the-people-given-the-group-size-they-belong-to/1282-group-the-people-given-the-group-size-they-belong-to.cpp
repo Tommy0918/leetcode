843244ae82d5e2767e988c760ccebc240ce2c1a6
rookieman <73873398+Tommy0918@users.noreply.github.com>
2024-02-29 18:32:36 +0800

    Time: 13 ms (30.36%), Space: 15.7 MB (55.78%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -5,7 +5,9 @@ public:
         vector<vector <int>> ans, all(n + 1);
         for (auto i = 0;i < n;i++){
             all[groupSizes[i]].push_back(i);
+            //cout << "1.groupSizes = " << groupSizes[i] << "  " << "i = " << i << "  " << "all.[groupSizes[i]].size() = " << all[groupSizes[i]].size() <<endl;
             if(all[groupSizes[i]].size() == groupSizes[i]){
+                //cout << "2.groupSizes = " << groupSizes[i] << endl;
                 ans.push_back(all[groupSizes[i]]);
                 all[groupSizes[i]].clear();
             }
