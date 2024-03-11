cdbd713f829dd9ad7b965894106568f25bddf272
rookieman <73873398+Tommy0918@users.noreply.github.com>
2024-03-11 10:26:53 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -3,7 +3,7 @@ public:
     string defangIPaddr(string address) {
         string ans;
         for(auto i : address){
-            if(i == '.')
+            if(&i == '.')
                 ans.append("[.]");
             else 
                 ans.push_back(i);
