cf85336ee5186cce51c53a075739252895760970
rookieman <73873398+Tommy0918@users.noreply.github.com>
2024-03-18 17:47:58 +0800

    Time: 8 ms (37.46%), Space: 23.7 MB (76.37%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int ans = 0;
-        int n = nums.size();
         for(auto i : nums){
             if(i < k)
                 ans++;
