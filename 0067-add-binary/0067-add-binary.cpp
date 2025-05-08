b91935fcee495ec0e46443a53e89fd09f5b60267
rookieman <73873398+Tommy0918@users.noreply.github.com>
2025-05-08 15:49:25 +0800

    Time: 0 ms (100%), Space: 8.8 MB (83.05%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0 || carry){
+            if(i >= 0) carry += a[i--] - '0';
+            if(j >= 0) carry += b[j--] - '0';
+
+            ans += carry % 2 + '0';
+            carry /= 2; 
+        }
+        reverse(begin(ans), end(ans));
+        return ans;
+    }
+};
